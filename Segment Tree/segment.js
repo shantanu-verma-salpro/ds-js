daff6b11f00e7dd73073bf17a1601e744c392eeb
shantanu-verma-salpro <shantanuwverma@gmail.com>
2023-07-03 17:46:51 +0530

    test

--- Segment Tree/segment.js
+++ Segment Tree/segment.js
@@ -18,3 +18,40 @@ function sum(x,v,tl,tr,l,r){
 }
 build([...A],1,0,A.length-1);
 console.log(sum(A,1,0,A.length-1,0,2))
+
+
+// Faster segment trees
+t = Array.from({ length : 2 * A.length+1 },()=>0);
+for(let i = 0;i < A.length; ++i) t[A.length+i] = A[i];
+t.build = ()=>{
+    for(let i = A.length-1;i>0;--i ){
+        t[i] = t[2 * i] + t[2 * i + 1];
+    }
+}
+t.update = (i,v)=>{
+    i = i + A.length;
+    t[i] = v;
+    while(i>1){
+        t[i>>1] = t[i] + t[i^1];
+        i>>=1;
+    }
+}
+t.query = (l,r)=>{
+    let n = A.length;
+    l += n;
+    r += n;
+    let res = 0;
+    while(l<r){
+        if(l&1){
+            res += t[l++];
+        }
+        if(r&1){
+            res += t[--r];
+        }
+        l>>=1;
+        r>>=1;
+    }
+    return res;
+}
+t.build();
+console.log(t.query(0,2));
